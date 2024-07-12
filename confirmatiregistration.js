8bd12766b81803661f070a14bb3fd2e90c57e4a2
Samuel <sammydave2716@gmail.com>
2024-07-12 12:33:37 -0500

    Committing changes

--- confirmatiregistration.js
+++ confirmatiregistration.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const confirmatiregistration = express();
 const fs = require('fs');
-const bodyParser = require('body-parser');
-//const excelJS = require('exceljs');
-
-confirmatiregistration.use(bodyParser.json());
-confirmatiregistration.use(bodyParser.urlencoded({extended: true}));
-
 const PORT = 4580;
+const ExcelJS = require('exceljs');
+const path =  require('path');
+const excelFile = path.join(__dirname, 'Manal COR and GPA for Applications that are stuck at - Application Status Check and Confirm_ATI_Registration Work Step - May 24, 2024.xlsx');
+const jsonFile  = path.join(__dirname, 'confirmatiregistration.json');
+
+confirmatiregistration.use(express.json());
+
 
 //Endpoint for port 4580
 confirmatiregistration.listen(PORT, () => {
@@ -15,12 +16,60 @@ confirmatiregistration.listen(PORT, () => {
 });
 
 
-               //ENDPOINTS TO GET, UPDATE, ADD AND DELETE DATA FOR EXCEL FILE/
-confirmatiregistration.get('/atistatus', function(req,res){
-    fs.readFile(__dirname + "/" + "confirmatiregistration.json", "utf8", function(errd,datab){
-        console.log(datab);
-        res.send(datab);
-        if (errd)
-            console.log(errd);
-    });
+//API Route to retrieve data from JSON File
+confirmatiregistration.get('/all-data', function(req,res){
+    try{
+        fs.readFile(jsonFile, function(errJsonfile, dataconaticreg){
+            console.log(dataconaticreg);
+            res.send(dataconaticreg);
+            if(errJsonfile){
+                console.log(errJsonfile);
+            }
+        });
+    }catch(geterr){
+        console.error(geterr);
+        res.status(404).send('Not Found')
+    }
+});
+
+//API route to add data to Excel Workbook
+applicationstatuscheck.post('/add-data', async (req, res) => {
+    try{
+        const newdata = req.body; //Expecting an array of objects
+        if (!newdata || !Array.isArray(newdata)) {
+            return res.status(400).send('Invalid format. Data should be in an array of objects.');
+        }
+        
+        try {
+            // Read existing JSON data
+            const jsonData = readJsonData();
+            
+            // Add new data to JSON
+            jsonData.push(...newdata);
+            writeJsonData(jsonData);
+            
+            // Open existing Excel file
+            const wkbk = new ExcelJS.Workbook();
+            await wkbk.xlsx.readFile(excelFile);
+            const wksht = wkbk.getWorksheet(2); //Using current worksheet
+            
+            // Adding new row/s to Excel worksheet
+            newdata.forEach(info => {
+            wksht.addRow(info);
+            });
+
+            // Save the Excel file
+            await wkbk.xlsx.writeFile(excelFile);
+
+             res.status(200).send('Data added successfully to the worksheet');
+    
+        }catch (excelerr) {
+        console.error(excelerr);
+        res.status(500).send('Data not added to the worksheet. Try again');
+        }
+
+    } catch(posterr){
+    console.error(posterr);
+    res.status(404).send('Not Found')
+   }
 });
--- payscorexcel.js
+++ payscorexcel.js
@@ -1,77 +1,91 @@
 const express = require('express');
 const payscorexcel = express();
 const fs = require('fs');
-const bodyParser = require('body-parser');
-//const excelJS = require('exceljs');
-
-payscorexcel.use(bodyParser.json());
-payscorexcel.use(bodyParser.urlencoded({extended: true}));
-
+const ExcelJS = require('exceljs');
+const path =  require('path');
 const PORT = 3080;
+const excelFile = path.join(__dirname, 'NCU-PAYS-COR-JUNE 19 2024 (003).xlsx');
+const jsonFile  = path.join(__dirname, 'NCU-PAYS-COR-JUNE 19 2024.json');
+
+payscorexcel.use(express.json());
 
 //Endpoint for port 3080
 payscorexcel.listen(PORT, () => {
     console.log("Server Listening on PORT: ", PORT);
 });
 
-               //ENDPOINTS TO GET, UPDATE, ADD AND DELETE DATA FPR EXCEL FILE/
 
-//Endpoint to retrieve customers' data from NCU-PAYS-COR-JUNE 19 2024
-payscorexcel.get('/customers', function(req,res){
-    fs.readFile(__dirname + "/" + "NCU-PAYS-COR-JUNE 19 2024.json", "utf8", function(err,data){
-        console.log(data);
-        res.send(data);
-        if (err)
-            console.log(err);
-    });
-});
+                    //Functions to read and write JSON Data - NCU-PAYS-COR-JUNE 19 2024.json
+//Function to read JSON File
+const readJsonData = () => {
+    if (fs.existsSync(jsonFile)) {
+        const data = fs.readFileSync(jsonFile);
+        return JSON.parse(data);
+    }
+    return [];
+};
+
+//Function to write JSON data
+const writeJsonData = (data) => {
+    fs.writeFileSync(jsonFile, JSON.stringify(data, null, 2));
+};
 
 
-//Endpoint to retrieve a customer's data by trn
-payscorexcel.get("/:customerbytrn", function(req,res){
-    fs.readFile(__dirname + "/" + "NCU-PAYS-COR-JUNE 19 2024.json", "utf8", function(err,data){
-        data = JSON.parse(data);
-        const trn = data + req.body.trn;
-        res.send(trn);
-        if (err)
-            console.log(err);
-    });
-});
-
-//Endpoint to update customer's data
-payscorexcel.put("/updatecustomer", function(req,res){
-    
-    fs.readFileSync(__dirname + "/" + "NCU-PAYS-COR-JUNE 19 2024.json", "utf8", function(err,data){
-        data = JSON.parse(data);
-        
-
-    });
-});
-
-//Endpoint to add customer's data to file
-payscorexcel.post("/addcustomer", function(req,res){
-    const newUser = {
-    };
-
-    fs.readFileSync(__dirname + "/" + "users.json", "utf8", function(err,data){
-        data = JSON.parse(data);
-        fs.writeFileSync(__dirname + "/" + "users.json", newUser, "utf8", err =>{
-            if (err){
-                console.error(err)
-            }else{
-                console.log(JSON.stringify(data));
-                res.send("Customer's data added successfully.");
+//API route to retrieve the data from JSON File
+payscorexcel.get('/all-data', async(req,res) =>{
+    try{
+        fs.readFile(jsonFile,  function(errJsonfile, datapayscor){
+            console.log(datapayscor);
+            res.send(datapayscor);
+            if(errJsonfile){
+                console.log(errJsonfile);
             }
-        })
-       if (err)
-            console.log(err);
-    });
+        });
+    }catch(geterr){
+        console.error(geterr);
+        res.status(404).send('Not Found')
+    }
 });
 
-//Endpoint to delete customer's data
-payscorexcel.delete("/deletecustomer", function(req,res){
-    fs.readFileSync(__dirname + "/" + "users.json", "utf8", function(err,data){
+//API route to add data to Excel Workbook
+payscorexcel.post('/add-data', async (req, res) => {
+     
+    try{
+        const newdata = req.body;
+        if (!newdata || !Array.isArray(newdata)) {
+            return res.status(400).send('Invalid format. Data should be in an array of objects.');
+        }
+        
+        try {
+            // Read existing JSON data
+            const jsonData = readJsonData();
+            
+            // Add new data to JSON
+            jsonData.push(...newdata);
+            writeJsonData(jsonData);
+            
+            // Open existing Excel file
+            const wkbk = new ExcelJS.Workbook();
+            await wkbk.xlsx.readFile(excelFile);
+            const wksht = wkbk.getWorksheet(1); //Using current worksheet
+            
+            // Adding new row/s to Excel worksheet
+            newdata.forEach(info => {
+            wksht.addRow(info);
+            });
 
-    });
+            // Save the Excel file
+            await wkbk.xlsx.writeFile(excelFile);
+
+             res.status(200).send('Data added successfully to the worksheet');
+    
+        }catch (excelerr) {
+        console.error(excelerr);
+        res.status(500).send('Data not added to the worksheet. Try again');
+        }
+
+    } catch(posterr){
+    console.error(posterr);
+    res.status(404).send('Not Found')
+   }
 });
-
