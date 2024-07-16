a8fe46ec55899ee5ada54b6ce643e4249de73bf7
Samuel <sammydave2716@gmail.com>
2024-07-16 16:33:56 -0500

    test code to add data to EXCEL FILE

--- test.js
+++ test.js
@@ -1,20 +1,30 @@
 var xlsx = require('xlsx')
 var wkbk = xlsx.readFile('NCU-PAYS-COR-JUNE 19 2024 (003).xlsx')
 
-var wsht = wkbk.Sheets['PAYS MAY 30 2024 (2)']
-var wsht = xlsx.utils.sheet_to_json(wsht)
- 
-wsht.push({
+//var wsht = wkbk.Sheets['PAYS MAY 30 2024 (2)']
+var wshtjson = xlsx.utils.sheet_to_json(wsht)
+
+var y =[{
     "Customer_TRN": "100291708",
     "RegistrationNo": "SLB-RLOS-123385",
-    "Registered": "Y",
-    "SemesterCost:": 181946.18,
+    "Registered": "YES",
+    "SemesterCost": 181946.18,
     "Semester": 1,
     "GPA": 3.79,
     "Product_Code": "",
     "CurrentAcademicYear": 2023
-  })
+    }]
 
-  xlsx.utils.sheet_add_json(wsht, wsht)
-  xlsx.writeFile(wkbk,'NCU-PAYS-COR-JUNE 19 2024 (003).xlsx')
+wshtjson.push(y)
+xlsx.utils.sheet_add_json(wkbk.Sheets['PAYS MAY 30 2024 (2)'], y, {origin: -1, skipHeader: true})
+xlsx.writeFile(wkbk,'NCU-PAYS-COR-JUNE 19 2024 (003).xlsx')
+
+
+
+
+
+
+
+
+
 
